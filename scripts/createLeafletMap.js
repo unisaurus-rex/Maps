696398f98d13ca0eb92e1f7111e156fd676699cb
Jeff Flower <jeffrey.flower@vantiv.com>
2016-10-05 14:56:41 -0600

    Make leaflet map a js module

--- scripts/createLeafletMap.js
+++ scripts/createLeafletMap.js
@@ -1,23 +1,8 @@
 // don't need to import leaflet because it's included as a cdn in index.html
-/*
-  export default function(mapContainerId) {
+
+export default function(mapContainerId) {
   // L = Leaflet.js object
   var map = L.map(mapContainerId);
-
-  map.options.minZoom = 4;
-  map.setView([37.8, -96.9], 4);
-
-  // add tile layer to map
-  L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: ['a','b','c']
-  }).addTo( map );
-
-  return map;
-  }
-*/
-function initMap() {
-  var map = L.map("mapContainer");
 
   map.options.minZoom = 4;
   map.setView([37.8, -96.9], 4);
@@ -27,4 +12,6 @@ function initMap() {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: ['a','b','c']
   }).addTo( map );
+
+  return map;
 }
--- scripts/startup.js
+++ scripts/startup.js
@@ -1,6 +1,8 @@
 import bootstrap from 'bootstrap-sass';
 import $ from 'jquery';
 import addToggle from './toggle';
+import initMap from './createLeafletMap'; 
 
 addToggle(); 
 
+var map = initMap("mapContainer");
