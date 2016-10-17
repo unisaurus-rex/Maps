6e81cc84bf8005a753bc1644afce11625e9771f0
Jeff Flower <jeffrey.flower@vantiv.com>
2016-10-17 14:57:11 -0600

    Refactor to make county widget modular

--- src/scripts/startup.js
+++ src/scripts/startup.js
@@ -1,16 +1,13 @@
 import bootstrap from 'bootstrap-sass';
-import $ from 'jquery';
 import addToggle from './toggle';
-import initMap from './createLeafletMap'; 
-import {us} from './json/variable-us'; 
-import drawRegions from './drawMapRegions';
-import {getD3ClassFunction} from './countySalesStyle';
-import {initTooltip, positionTooltip, hideTooltip, addTooltipHTML} from './tooltip.js';
+import initMap from './createLeafletMap';
+import{createCountyWidget} from './countyWidget';
 
-addToggle(); 
+addToggle();
 
 var mapCenter = [39.7, -104.99];
 var initialMapZoom = 7;
+var map1 = initMap("div.mapPanel", "mapContainer", mapCenter, initialMapZoom);
 var salesData = {
   keys: ["18-34", "35-54", "55+"],
   data: [
@@ -25,84 +22,12 @@ var salesData = {
     }
   ]
 };
+var countyConfig = {
+  salesData: salesData,
+  countyFilePath: 'src/scripts/json/counties.json',
+  leafletMap: map1,
+  tooltipId: "mapTooltip1",
+  tooltipClass: "mapTooltip"
+};
 
-var map1 = initMap("div.mapPanel", "mapContainer", mapCenter, initialMapZoom);
-
-
-$.getJSON('src/scripts/json/counties.json', function(data){
-  var sales = salesData.data;
-  var regions = data.features;
-  
-  // merge sales data with geojson data
-  for(var i = 0; i < salesData.data.length; i++){
-    var region = sales[i].region;
-    var salesFigures = sales[i].sales;
-    for(var j = 0; j < regions.length; j++){
-      if(regions[j].properties.NAME == region){
-        regions[j].saleKeys = salesData.keys;
-        regions[j].sales = salesFigures;
-      }
-    } 
-  }
-
-  drawRegions(map1, regions, "countypath");
-
-  // select all county paths
-  var counties = d3.selectAll("path.countypath");
-  
-  // add hidden tooltip
-  initTooltip();
-
-  // style regions
-  // var d3func = getD3ClassFunction(["18-34","35-54", "55+"]);
-  // counties.attr("class", d3func);
-  styleCountyBySales(["18-34","35-54", "55+"]);
-
-  // show/hide tooltip on hover
-  counties.on("mouseover", function(d) {
-    var tooltipBody = `
-         <table>
-         <thead><tr><th>Group</th><th>Sales</th></tr></thead>
-         <tbody>
-         <tr><td>18-34</td><td>$ ${d.sales[0]}</td></tr>
-         <tr><td>35-54</td><td>$ ${d.sales[1]}</td></tr>
-         <tr><td>55+</td><td>$ ${d.sales[2]}</td></tr>
-         </tbody>
-         </table>`
-
-    positionTooltip(d3.mouse(this));
-    addTooltipHTML(tooltipBody);
-  });
-  counties.on("mouseout", hideTooltip);
-  
-  // on checkbox click update the color style for each region
-  $("input[name=demographics]").on("click", function(){
-    var selectedDemographics = getCheckedValues("demographics");
-    styleCountyBySales(selectedDemographics);
-  });
-  
-});
-
-// get checked values as an array
-function getCheckedValues(checkboxName){
-  var selector = "input[name=" + checkboxName + "]:checked";
-  // calling .get() on a jquery object returns a plain array of elements
-  return $(selector).get().map(function(el){return el.value;});
-}
-
-// given an array of demographics ranges, style region by the combined sales of the demographics
-function styleCountyBySales(demographics){
-  // build a function that d3 can use to apply a style to an element
-  var d3func = getD3ClassFunction(demographics);
-  
-  var counties = d3.selectAll("path.countypath");
-  
-  // remove any sales color classes before applying new ones
-  counties.classed("lowSales avgSales highSales", false);
-
-  // apply style function to each county region
-  d3.selectAll("path.countypath").attr("class", d3func);
-}
-
-
-
+createCountyWidget(countyConfig);
--- src/scripts/tooltip.js
+++ src/scripts/tooltip.js
@@ -1,22 +1,23 @@
-export function initTooltip() {
+export function initTooltip(tooltipId, tooltipClass) {
   d3.select("body")
     .append('div')
+    .attr("id", tooltipId)
     .classed('hidden', true)
-    .classed('mapTooltip', true);
+    .classed(tooltipClass, true);
 }
 
 // tooltipCenter is array[how far right, how far down]
-export function positionTooltip(tooltipCenter) {
-  d3.select('.mapTooltip')
+export function positionTooltip(tooltipId, tooltipCenter) {
+  d3.select("#" + tooltipId)
     .classed('hidden', false)
     .style('left', tooltipCenter[0] + "px")
     .style('top', tooltipCenter[1] + "px");
 }
 
-export function hideTooltip(){
-  d3.select('.mapTooltip').classed('hidden', true);
+export function hideTooltip(tooltipId){
+  d3.select("#" + tooltipId).classed('hidden', true);
 }
 
-export function addTooltipHTML(htmlStr){
-  d3.select('.mapTooltip').html(htmlStr);
+export function addTooltipHTML(tooltipId, htmlStr){
+  d3.select("#" + tooltipId).html(htmlStr);
 }
